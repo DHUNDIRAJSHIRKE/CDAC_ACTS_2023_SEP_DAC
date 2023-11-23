d414a2d4fb2a9e3484697ae14cf6bc4dc1c89300
DHUNDIRAJSHIRKE <65431888+DHUNDIRAJSHIRKE@users.noreply.github.com>
2023-11-23 21:35:40 +0530

    After lab day 8

---
+++ Web_Based_Java/NewProject/src/main/java/tester/HelloWorldTester.java
@@ -0,0 +1,18 @@
+package tester;
+//import static utils.HibernateUtils.getFactory;
+import org.hibernate.SessionFactory;
+
+public class HelloWorldTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		try(SessionFactory sf=getSf())
+//		{
+//			System.out.println("Hibernate booted.....");
+//		}catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		System.out.println("Hello World!!");
+	}
+
+}

d64a23f889c06afb43cf20ce717d347f4446e9e1
DHUNDIRAJSHIRKE <DHUNDIRAJSHIRKE.DS@GMAIL.COM>
2023-12-06 07:53:21 +0530

    Starting first lab of .NET

---
+++ .NET/ConsoleApp_054/ConsoleApp_054_01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace N1 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            N2.Program.F1();
+        }
+    }
+    namespace N2
+    {
+        class Program
+        {
+            public static void F1()
+            {
+                Console.WriteLine("Hello from namespace N2's Program class!!");
+            }
+        }
+    }
+}
---
+++ Web_Based_Java/day17_REST3_lab17/src/main/java/com/app/dao/StudentDao.java
@@ -0,0 +1,14 @@
+package com.app.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.app.entities.Student;
+
+public interface StudentDao extends JpaRepository<Student, Long>{
+
+	//for adding student : save
+	
+	
+	
+	
+}

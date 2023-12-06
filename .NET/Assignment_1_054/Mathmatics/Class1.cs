222375c1149647f1dd4816799ee2bec56b029c44
DHUNDIRAJSHIRKE <DHUNDIRAJSHIRKE.DS@GMAIL.COM>
2023-12-06 21:34:47 +0530

    After lab day 2

---
+++ .NET/Assignment_1_054/Mathmatics/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Mathmatics
+{
+    public class maths
+    {
+        public static decimal getNetSalary(decimal basic)
+        {
+            return basic * 100 + 1000;
+        }
+    }
+}

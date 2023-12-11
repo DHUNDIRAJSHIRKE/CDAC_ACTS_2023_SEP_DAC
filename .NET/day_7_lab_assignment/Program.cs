c888c5d0fdc36715345e987a8cc02eb364b8b8e4
DHUNDIRAJSHIRKE <DHUNDIRAJSHIRKE.DS@GMAIL.COM>
2023-12-11 08:36:30 +0530

    Before lab day 7

---
+++ .NET/day_7_lab_assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace day_7_lab_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a String: ");
+            string? str = Console.ReadLine();
+            Console.WriteLine(str.CountVowel());
+
+        }
+        
+    }
+    public static class StringExt {
+        public static int CountVowel(this string ob)
+        {
+            int count = 0;
+            char[] chars = ob.ToCharArray();
+            foreach (var item in chars)
+            {
+                if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' || item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}

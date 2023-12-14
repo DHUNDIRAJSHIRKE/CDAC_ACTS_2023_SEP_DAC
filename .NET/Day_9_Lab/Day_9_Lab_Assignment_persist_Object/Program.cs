8df72e6d99b846645aa0f5b4520aeb0a7b6a2e13
DHUNDIRAJSHIRKE <DHUNDIRAJSHIRKE.DS@GMAIL.COM>
2023-12-14 11:16:01 +0530

    after lab day 9

---
+++ .NET/Day_9_Lab/Day_9_Lab_Assignment_persist_Object/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Day_9_Lab_Assignment_persist_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ser();
+            deser();
+        }
+        public static void ser()
+        {
+            Employee employee = new Employee();
+            employee.Name = "Nitin";
+            employee.Salary = 10500;
+            employee.Id = 1;
+            BinaryFormatter bf = new BinaryFormatter();
+            Stream s = new FileStream("D:\\CDAC_ACTS_2023_SEP_DAC\\.NET\\Day_9_Lab\\proj1.txt",FileMode.Create);
+#pragma warning disable SYSLIB0011 // Type or member is obsolete
+            bf.Serialize(s, employee);
+#pragma warning restore SYSLIB0011
+
+            s.Close();
+
+        }
+        public static void deser()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            Stream s = new FileStream("D:\\CDAC_ACTS_2023_SEP_DAC\\.NET\\Day_9_Lab\\proj1.txt", FileMode.Open);
+#pragma warning disable SYSLIB0011
+            Employee e2 = (Employee)bf.Deserialize(s);
+#pragma warning restore SYSLIB0011
+            Console.WriteLine(e2.Name);
+            Console.WriteLine(e2.Salary);
+            Console.WriteLine(e2.Id);
+            s.Close();
+        }
+    }
+
+    [Serializable]
+    public class Employee :ISerializable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", Id);
+            info.AddValue("sal", Salary);
+            info.AddValue("name", Name);
+        }
+        public Employee()
+        {
+
+        }
+        public Employee(SerializationInfo info, StreamingContext context) 
+        {
+            Id = info.GetInt32("id");
+            Name = info.GetString("name");
+            Salary = info.GetDouble("sal");
+        }
+    }
+
+}
---
+++ .NET/Day_9_Lab/XML_Serialization/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Runtime.Serialization;
+namespace XML_Serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            ser();
+            deser();
+        }
+        public static void ser()
+        {
+            Employee employee = new Employee();
+            employee.Name = "Nitin";
+            employee.Salary = 10500;
+            employee.Id = 1;
+            SoapFormatter sf = new SoapFormatter();
+            Stream s = new FileStream("D:\\CDAC_ACTS_2023_SEP_DAC\\.NET\\Day_9_Lab\\proj2.txt", FileMode.Create);
+            sf.Serialize(s, employee);
+            s.Close();
+        }
+        public static void deser()
+        {
+            SoapFormatter sf = new SoapFormatter();
+            Stream s = new FileStream("D:\\CDAC_ACTS_2023_SEP_DAC\\.NET\\Day_9_Lab\\proj2.txt", FileMode.Open);
+            Employee e2 = (Employee)sf.Deserialize(s);
+            s.Close ();
+            Console.WriteLine(e2.Name);
+            Console.WriteLine(e2.Id);
+            Console.WriteLine(e2.Salary);
+        }
+    }
+
+
+
+    [Serializable]
+    public class Employee : ISerializable
+    {
+        public int Id { get; set; }
+        [XmlAttribute]
+        public string Name { get; set; }
+        [XmlAttribute]
+        public double Salary { get; set; }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("id", Id);
+            info.AddValue("sal", Salary);
+            info.AddValue("name", Name);
+        }
+        public Employee()
+        {
+
+        }
+        public Employee(SerializationInfo info, StreamingContext context)
+        {
+            Id = info.GetInt32("id");
+            Name = info.GetString("name");
+            Salary = info.GetDouble("sal");
+        }
+    }
+}

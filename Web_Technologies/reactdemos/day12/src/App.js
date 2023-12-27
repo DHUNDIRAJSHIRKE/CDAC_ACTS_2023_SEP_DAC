04ca5dcdc77a629f0da3738a64174ff5b4d0d634
DHUNDIRAJSHIRKE <DHUNDIRAJSHIRKE.DS@GMAIL.COM>
2023-12-27 18:02:44 +0530

    before lecture day 12

---
+++ Web_Technologies/reactdemos/day12/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "../src/bootstrap-5.0.2-dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CounterClassComponent from "./components/CounterClassComponent";
+import CounterfucntionalComponent from "./components/CounterfunctionalComponent";
+import DisplayNames from "./components/DisplayNames";
+function App() {
+  let namearr = ["Revati", "Atharva", "Sohuum", "Rajashree"];
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Header></Header>
+
+      <CounterClassComponent></CounterClassComponent>
+      <hr></hr>
+      <CounterfucntionalComponent></CounterfucntionalComponent>
+      <DisplayNames nmarr={namearr}></DisplayNames>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
